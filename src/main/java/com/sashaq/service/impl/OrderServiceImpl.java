694663a9c489ee3e13aa1ab6c90f2e6764ec5f11
SashaQ <sasha@itictory.com>
2017-05-02 17:38:39 +0300

    orderServiceImpl

---
+++ src/main/java/com/sashaq/service/impl/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.sashaq.service.impl;
+
+import com.sashaq.service.OrderService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+}
