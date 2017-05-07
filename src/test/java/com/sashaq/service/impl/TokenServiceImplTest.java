806e511778d3be8132f35cee6b44de7c07a4a332
sergio <sergio5990kruk@gmail.com>
2017-05-07 22:40:30 +0300

    delete test

--- src/test/java/com/sashaq/service/impl/TokenServiceImplTest.java
+++ src/test/java/com/sashaq/service/impl/TokenServiceImplTest.java
@@ -1,33 +1,18 @@
 package com.sashaq.service.impl;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.sashaq.service.TokenService;
 import org.junit.Test;
 
-import java.math.BigInteger;
-import java.security.SecureRandom;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
-import static org.junit.Assert.*;
-
-/**
- * Created by SashaQ on 27.04.2017.
- */
 public class TokenServiceImplTest {
-    TokenServiceImpl tokenService = new TokenServiceImpl();
+    private TokenService tokenService = new TokenServiceImpl();
 
     @Test
     public void buildNewToken(){
         String token = tokenService.buildNewToken("123");
         assertNotNull(token);
-        System.out.println(token);
-    }
-
-    @Test
-    public void name() {
-        SecureRandom random = new SecureRandom();
-        String s = new BigInteger(512, random).toString(32).toUpperCase();
-        System.out.println(new BigInteger(512, random).toString(32).toUpperCase());
-        assertNotNull(s);
-        System.out.println(s);
+        assertFalse(token.isEmpty());
     }
 }
